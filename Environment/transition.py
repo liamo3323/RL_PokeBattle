441d853e16fe0f2ee4aad8f306bf913511898fe3
liam <liam3323@gmail.com>
2023-06-29 20:17:26 +0100

    added transition

--- Environment/transition.py
+++ Environment/transition.py
@@ -2,16 +2,15 @@ class PokemonRLBattler():
     def __init__(self):
         self.temp 
 
-    def step():
+    def step(): # single timestep method
         return None
     
-    def reset():
+    def reset(): # ran at the start of the environment and at the end of each episode
         return None
     
     #TODO Later after barebones is done
     def render():
         return None
 
-    def close():
+    def close(): # ???
         return None
-    
